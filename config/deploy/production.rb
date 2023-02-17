17bac0feca552168eb559cb04db52b6aa20ae7d7
Arie <kelivdc@yahoo.com>
2023-02-17 20:44:13 +0700

    Deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server 'smartdropbox.online', user: 'deploy', roles: %w{app db}
 
 # role-based syntax
 # ==================
