d75f1630dd0f6e38fc19db828e115b190324bc9d
Arie <kelivdc@yahoo.com>
2023-02-20 21:35:08 +0700

    Update cloudinary

--- app/blueprints/botol_blueprint.rb
+++ app/blueprints/botol_blueprint.rb
@@ -9,8 +9,8 @@ class BotolBlueprint < Blueprinter::Base
             image_urls = img.images.map do |image|
                 if image
                     {
-                        thumb: image.variant(:thumb).url,
-                        full: image.variant(:full).url,
+                        thumb: Cloudinary::Utils.cloudinary_url(image.key, :width => 200, :height => 200, :crop => :fill),
+                        full: Cloudinary::Utils.cloudinary_url(image.key),
                     }        
                 end    
             end
@@ -19,8 +19,8 @@ class BotolBlueprint < Blueprinter::Base
 
     view :index do
         field :images do |img|
-            if img.images[0].variant(:thumb).url
-                img.images[0].variant(:thumb).url
+            if img.images[0]
+                Cloudinary::Utils.cloudinary_url(img.images[0].key, :width => 200, :height => 200, :crop => :fill)
             else
                 "-"
             end
