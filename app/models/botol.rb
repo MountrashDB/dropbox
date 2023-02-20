3298e5508310fabcb4ae580b0cf5acabdfa7750a
Arie <kelivdc@yahoo.com>
2023-02-20 21:38:58 +0700

    Update model Botol, remove variant

--- app/models/botol.rb
+++ app/models/botol.rb
@@ -1,9 +1,10 @@
 class Botol < ApplicationRecord
   belongs_to :merk  
-  has_many_attached :images, dependent: :destroy, service: :cloudinary do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
-    attachable.variant :full, resize_to_limit: [500, 500]
-  end
+  has_many_attached :images, dependent: :destroy, service: :cloudinary 
+  # do |attachable|
+  #   attachable.variant :thumb, resize_to_limit: [100, 100]
+  #   attachable.variant :full, resize_to_limit: [500, 500]
+  # end
   before_create :set_uuid
 
   def set_uuid
