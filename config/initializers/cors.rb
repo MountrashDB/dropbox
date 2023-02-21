5296e821d66db6d7bf0fc7104700871aab9404a9
Arie <kelivdc@yahoo.com>
2023-02-21 19:05:36 +0700

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "admin.smartdropbox.online"
+    origins "admin.smartdropbox.online, api.smartdropbox.online"
 
     resource "*",
       headers: :any,
