6e3bb7b5be84e5d87b2c23f4d553ec449fdd77cc
Arie <kelivdc@yahoo.com>
2023-03-06 23:10:12 +0700

    Update box

--- app/controllers/api/v1/boxes_controller.rb
+++ app/controllers/api/v1/boxes_controller.rb
@@ -37,12 +37,23 @@ class Api::V1::BoxesController < AdminController
 
   def update    
     if box = Box.find_by(uuid: params[:uuid])
-      box.qr_code = params[:qr_code]
-      box.lat = params[:lat]
-      box.lang = params[:lang]
-      box.max = params[:max]
-      box.save
-      render json: box
+      box.cycles = params[:cycles]
+      box.dates = params[:dates]
+      box.jenis = params[:jenis]
+      box.nama = params[:nama]
+      box.qty = params[:qty]
+      box.revenue = params[:revenue]
+      box.mitra_share = params[:mitra_share]    
+      box.user_share = params[:user_share]    
+      box.latitude = params[:latitude]
+      box.longitude = params[:longitude]    
+      box.admin_id = @current_admin.id
+      box.mitra_id = Mitra.find_by(uuid: params[:mitra_uuid]).id
+      if box.save
+        render json: BoxBlueprint.render(box)
+      else
+        render json: {error: box.errors}
+      end
     else
       render json: {message: "Not found"}, status: :not_found
     end    
