2d70a7551186a89f50a50184a0714c2b230b5ec6
Arie <kelivdc@yahoo.com>
2023-03-14 00:11:36 +0700

    Foto notify

--- app/models/usertransaction.rb
+++ app/models/usertransaction.rb
@@ -25,6 +25,6 @@ class Usertransaction < ApplicationRecord
   after_create :send_notify
 
   def send_notify    
-    NotifyChannel.broadcast_to self.user.uuid, status: "complete", image: Cloudinary::Utils.cloudinary_url(self.foto.key, :width => 200, :height => 200, :crop => :fill), diterima: true
+    NotifyChannel.broadcast_to self.user.uuid, status: "complete", image: Cloudinary::Utils.cloudinary_url(Transaction.last.foto.key, :width => 200, :height => 200, :crop => :fill), diterima: true
   end
 end
