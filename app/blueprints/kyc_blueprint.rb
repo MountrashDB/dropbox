a55ba13c6c75324ba4b9ef0f4be5759dafb128a9
Arie <kelivdc@yahoo.com>
2023-03-15 22:16:58 +0700

    KYC image

--- app/blueprints/kyc_blueprint.rb
+++ app/blueprints/kyc_blueprint.rb
@@ -15,4 +15,12 @@ class KycBlueprint < Blueprinter::Base
     association :province, blueprint: ProvinceBlueprint
     association :city, blueprint: CityBlueprint
     association :district, blueprint: DistrictBlueprint
+
+    field :ktp_image do |data|
+        if data
+            data.ktp_image.url
+        else
+            "-"
+        end
+    end
 end
