868024e1558704efdac562a3bc1862f88b4626ff
Arie <kelivdc@yahoo.com>
2023-04-07 00:06:42 +0700

    Transaction sort Box datatable

--- app/datatables/transaction_datatable.rb
+++ app/datatables/transaction_datatable.rb
@@ -1,19 +1,18 @@
 class TransactionDatatable < AjaxDatatablesRails::ActiveRecord
-
   def view_columns
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
     @view_columns ||= {
       uuid: { source: "Transaction.id", cond: :eq, searchable: true },
-      diterima: { source: "Transaction.diterima", cond: :eq, searchable: true},
-      harga: { source: "Transaction.harga", cond: :eq},
-      mitra_amount: { source: "Transaction.mitra_amount"}, 
-      user_amount: { source: "Transaction.user_amount"}, 
-      box: { source: "Box.nama", cond: :like, searchable: true}, 
-      mitra: { source: "Mitra.name", cond: :like, searchable: true},
-      user: { source: "User.name", cond: :like, searchable: true},
-      foto: { source: "Transaction.foto"},
-      created_at: { source: "Transaction.created_at", cond: :like, searchable: true}
+      diterima: { source: "Transaction.diterima", cond: :eq, searchable: true },
+      harga: { source: "Transaction.harga", cond: :eq },
+      mitra_amount: { source: "Transaction.mitra_amount" },
+      user_amount: { source: "Transaction.user_amount" },
+      box: { source: "Box.nama", cond: :like, searchable: true },
+      mitra: { source: "Mitra.name", cond: :like, searchable: true },
+      user: { source: "User.name", cond: :like, searchable: true },
+      foto: { source: "Transaction.foto" },
+      created_at: { source: "Transaction.created_at", cond: :like, searchable: true },
     }
   end
 
@@ -21,7 +20,7 @@ class TransactionDatatable < AjaxDatatablesRails::ActiveRecord
   #   @mitra_id = opts[:mitra_id]
   # end
 
-  def data    
+  def data
     records.map do |record|
       {
         uuid: record.uuid,
@@ -33,18 +32,18 @@ class TransactionDatatable < AjaxDatatablesRails::ActiveRecord
         mitra: record.mitra.name,
         user: record.user.name,
         image: record.foto.url,
-        created_at: record.created_at
+        created_at: record.created_at,
       }
     end
-  end 
+  end
 
-  def get_raw_records   
+  def get_raw_records
     if options[:mitra_id]
-      Transaction.where(mitra_id: options[:mitra_id])
+      Transaction.where(mitra_id: options[:mitra_id]).joins(:box)
     elsif options[:user_id]
-      Transaction.where(user_id: options[:user_id])      
+      Transaction.where(user_id: options[:user_id]).joins(:box)
     else
-      Transaction.all
+      Transaction.all.joins(:box)
     end
   end
 end
