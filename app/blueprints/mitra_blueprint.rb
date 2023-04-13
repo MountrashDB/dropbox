f3a6f4f12810299997dcf7d38744dc71370b9de4
Arie <kelivdc@yahoo.com>
2023-04-13 23:32:14 +0700

    Mitra blueprint

--- app/blueprints/mitra_blueprint.rb
+++ app/blueprints/mitra_blueprint.rb
@@ -1,32 +1,33 @@
 class MitraBlueprint < Blueprinter::Base
-    identifier :uuid
+  identifier :uuid
 
+  fields :name
+  view :register do
+    fields :email, :activation_code, :phone
+    association :partner, blueprint: PartnerBlueprint
+  end
+
+  view :profile do
+    fields :email, :activation_code, :phone
+    field :image do |data|
+      if data
+        data.image.url
+      else
+        "-"
+      end
+    end
+  end
+
+  view :after_update do
+    fields :email, :phone
+  end
+
+  view :admin_index do
     fields :name
-    view :register do
-        fields :email, :activation_code, :phone
-    end
+  end
 
-    view :profile do        
-        fields :email, :activation_code, :phone
-        field :image do |data|
-            if data
-                data.image.url
-            else
-                "-"
-            end
-        end
-    end
-
-    view :after_update do
-        fields :email, :phone
-    end
-
-    view :admin_index do
-        fields :name
-    end
-
-    view :show do
-        fields :email, :phone
-        association :kyc, blueprint: KycBlueprint
-    end
+  view :show do
+    fields :email, :phone
+    association :kyc, blueprint: KycBlueprint
+  end
 end
