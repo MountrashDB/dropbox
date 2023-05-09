d58c9fe7710effa0b8d8dab4e745445650f59000
Arie <kelivdc@yahoo.com>
2023-05-09 10:33:42 +0700

    User balance

--- app/datatables/user_datatable.rb
+++ app/datatables/user_datatable.rb
@@ -8,6 +8,7 @@ class UserDatatable < AjaxDatatablesRails::ActiveRecord
       email: { source: "User.email", cond: :like, searchable: true },
       uuid: { source: "User.uuid", cond: :like },
       active: { source: "User.active", cond: :eq },
+      balance: { source: "User.usertransactions.balance" },
       created_at: { source: "User.created_at", cond: :eq },
     }
   end
@@ -21,6 +22,7 @@ class UserDatatable < AjaxDatatablesRails::ActiveRecord
         email: record.email,
         active: record.active,
         created_at: record.created_at,
+        balance: record.usertransactions.balance,
       }
     end
   end
