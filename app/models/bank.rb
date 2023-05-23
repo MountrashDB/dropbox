3e3d0088584114aeeba821c67ff55eee2cc62dd9
Arie <kelivdc@yahoo.com>
2023-05-23 22:53:03 +0700

    Update Bank model

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: banks
+#
+#  id         :bigint           not null, primary key
+#  is_active  :boolean
+#  kode_bank  :string(255)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Bank < ApplicationRecord
 end
