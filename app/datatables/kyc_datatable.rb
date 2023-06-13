9b4d991e48c1c356d18a2ddb6418000cb5a5c4ae
Arie <kelivdc@yahoo.com>
2023-06-13 18:59:26 +0700

    Add mitra to KYC datatable

--- app/datatables/kyc_datatable.rb
+++ app/datatables/kyc_datatable.rb
@@ -1,5 +1,4 @@
 class KycDatatable < AjaxDatatablesRails::ActiveRecord
-
   def view_columns
     # Declare strings in this format: ModelName.column_name
     # or in aliased_join_table.column_name format
@@ -11,7 +10,7 @@ class KycDatatable < AjaxDatatablesRails::ActiveRecord
       desa: { source: "Kyc.desa", cond: :like, searchable: true },
       tempat_tinggal: { source: "Kyc.tempat_tinggal", cond: :like, searchable: true },
       no_ktp: { source: "Kyc.no_ktp", cond: :like, searchable: true },
-      mitra: { source: "Mitra.name", cond: :like, searchable: true }
+      mitra: { source: "Mitra.name", cond: :like, searchable: true },
     }
   end
 
@@ -21,15 +20,14 @@ class KycDatatable < AjaxDatatablesRails::ActiveRecord
         uuid: record.uuid,
         nama: record.nama,
         tempat_tinggal: record.tempat_tinggal,
-        status: record.status,        
+        status: record.status,
         no_ktp: record.no_ktp,
-        mitra: record.mitra.name
+        mitra: record.mitra.name,
       }
     end
   end
 
   def get_raw_records
-    Kyc.all
+    Kyc.all.includes(:mitra)
   end
-
 end
