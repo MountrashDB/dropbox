670f548ed50cb4c9a4d0915e518454a1e99249f5
Arie <kelivdc@yahoo.com>
2023-07-12 21:50:04 +0700

    Botol ukuran

--- app/blueprints/botol_blueprint.rb
+++ app/blueprints/botol_blueprint.rb
@@ -1,17 +1,17 @@
 class BotolBlueprint < Blueprinter::Base
   identifier :uuid
 
-  fields :name, :jenis, :product, :barcode
+  fields :name, :jenis, :product, :barcode, :ukuran
 
   view :show do
     field :images do |img|
       image_urls = img.images.map do |image|
         if image
           {
-                      id: image.id,
-                      thumb: Cloudinary::Utils.cloudinary_url(image.key, :width => 300, :height => 300, :crop => :fill),
-                      full: Cloudinary::Utils.cloudinary_url(image.key),
-                    }
+            id: image.id,
+            thumb: Cloudinary::Utils.cloudinary_url(image.key, :width => 300, :height => 300, :crop => :fill),
+            full: Cloudinary::Utils.cloudinary_url(image.key),
+          }
         end
       end
     end
