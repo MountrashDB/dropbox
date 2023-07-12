53c5c94ec2f063169b7602ab268059df9131271b
Arie <kelivdc@yahoo.com>
2023-07-12 21:15:19 +0700

    Botol blueprint

--- app/blueprints/botol_blueprint.rb
+++ app/blueprints/botol_blueprint.rb
@@ -1,29 +1,29 @@
 class BotolBlueprint < Blueprinter::Base
-    identifier :uuid
+  identifier :uuid
 
-    fields :name
-    
-    view :show do
-        field :images do |img|
-            image_urls = img.images.map do |image|
-                if image
-                    {
-                        id: image.id,
-                        thumb: Cloudinary::Utils.cloudinary_url(image.key, :width => 200, :height => 200, :crop => :fill),
-                        full: Cloudinary::Utils.cloudinary_url(image.key),
-                    }        
-                end    
-            end
-        end
-    end
+  fields :name, :jenis, :product, :barcode
 
-    view :index do
-        field :images do |img|
-            if img.images[0]
-                Cloudinary::Utils.cloudinary_url(img.images[0].key, :width => 200, :height => 200, :crop => :fill)
-            else
-                "-"
-            end
+  view :show do
+    field :images do |img|
+      image_urls = img.images.map do |image|
+        if image
+          {
+                      id: image.id,
+                      thumb: Cloudinary::Utils.cloudinary_url(image.key, :width => 300, :height => 300, :crop => :fill),
+                      full: Cloudinary::Utils.cloudinary_url(image.key),
+                    }
         end
+      end
     end
+  end
+
+  view :index do
+    field :images do |img|
+      if img.images[0]
+        Cloudinary::Utils.cloudinary_url(img.images[0].key, :width => 200, :height => 200, :crop => :fill)
+      else
+        "-"
+      end
+    end
+  end
 end
