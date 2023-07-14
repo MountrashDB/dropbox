051d224c2b090543d636ba6183e0c639692a2271
Arie <kelivdc@yahoo.com>
2023-07-14 14:14:00 +0700

    Box blueprint

--- app/blueprints/box_blueprint.rb
+++ app/blueprints/box_blueprint.rb
@@ -1,23 +1,25 @@
 class BoxBlueprint < Blueprinter::Base
-    identifier :uuid
+  identifier :uuid
 
-    fields :nama, 
-        :uuid,
-        :qr_code,
-        :qty,
-        :cycles, 
-        :dates, 
-        :jenis, 
-        :latitude, 
-        :longitude, 
-        :mitra_share, 
-        :user_share, 
-        :type_progress,
-        :created_at,
-        :updated_at
-    association :mitra, blueprint: MitraBlueprint
+  fields :nama,
+         :uuid,
+         :qr_code,
+         :qty,
+         :cycles,
+         :dates,
+         :jenis,
+         :latitude,
+         :longitude,
+         :mitra_share,
+         :user_share,
+         :type_progress,
+         :created_at,
+         :updated_at,
+         :price_pcs,
+         :price_kg
+  association :mitra, blueprint: MitraBlueprint
 
-    view :show_harga do
-        excludes :qr_code, :qty, :cycles,  :dates, :jenis, :latitude, :longitude, :mitra_share, :user_share, :type_progress, :created_at, :updated_at
-    end
+  view :show_harga do
+    excludes :qr_code, :qty, :cycles, :dates, :jenis, :latitude, :longitude, :mitra_share, :user_share, :type_progress, :created_at, :updated_at
+  end
 end
