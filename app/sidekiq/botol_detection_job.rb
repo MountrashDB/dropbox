1e5f26d324a3a93cb3dfdc7685739740d28dfd5b
Arie <kelivdc@yahoo.com>
2023-08-09 12:34:47 +0700

    Diterima semua dulu

--- app/sidekiq/botol_detection_job.rb
+++ app/sidekiq/botol_detection_job.rb
@@ -8,37 +8,37 @@ class BotolDetectionJob
     Rails.logger.error "=== Args ==="
     Rails.logger.error args
     Rails.logger.error "=== Checking ==="
-    begin
-      confidence = result["info"]["detection"]["object_detection"]["data"]["coco"]["tags"]["bottle"][0]["confidence"]
-      confidence = 0.8 # Untuk sementara
-      if confidence > 0.5
-        transaction.diterima = true
-      else
-        Rails.logger.error "=== Rejected ==="
-
-        transaction.mitra_amount = 0
-        transaction.user_amount = 0
-        transaction.diterima = false
-        NotifyChannel.broadcast_to transaction.user.uuid,
-                                   status: "complete",
-                                   image: transaction.foto.url,
-                                   point: 0,
-                                   diterima: false,
-                                   message: "Rejected"
-      end
-    rescue Exception => e
-      Rails.logger.error "=== Error Detection ==="
-      Rails.logger.error e
-      transaction.mitra_amount = 0
-      transaction.user_amount = 0
-      NotifyChannel.broadcast_to transaction.user.uuid,
-                                 status: "complete",
-                                 image: transaction.foto.url,
-                                 point: 0,
-                                 diterima: false,
-                                 message: "Rejected"
-      transaction.diterima = false
-    end
+    # begin
+    confidence = result["info"]["detection"]["object_detection"]["data"]["coco"]["tags"]["bottle"][0]["confidence"]
+    transaction.diterima = true
+    #   confidence = 0.8 # Untuk sementara
+    #   if confidence > 0.5
+    #     transaction.diterima = true
+    #   else
+    #     Rails.logger.error "=== Rejected ==="
+    #     transaction.mitra_amount = 0
+    #     transaction.user_amount = 0
+    #     transaction.diterima = false
+    #     NotifyChannel.broadcast_to transaction.user.uuid,
+    #                                status: "complete",
+    #                                image: transaction.foto.url,
+    #                                point: 0,
+    #                                diterima: false,
+    #                                message: "Rejected"
+    #   end
+    # rescue Exception => e
+    #   Rails.logger.error "=== Error Detection ==="
+    #   Rails.logger.error e
+    #   transaction.mitra_amount = 0
+    #   transaction.user_amount = 0
+    #   NotifyChannel.broadcast_to transaction.user.uuid,
+    #                              status: "complete",
+    #                              image: transaction.foto.url,
+    #                              point: 0,
+    #                              diterima: false,
+    #                              message: "Rejected"
+    #   transaction.diterima = false
+    # end
     transaction.save
   end
 end
