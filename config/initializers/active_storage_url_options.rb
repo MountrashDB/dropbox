1185d4d16bbacf89cca3bc89b4b46194f463b16e
Arie A <dropbox@mountrash.com>
2023-09-09 20:19:47 +0700

    activeStorage

---
+++ config/initializers/active_storage_url_options.rb
@@ -0,0 +1,4 @@
+ActiveStorage::Current.url_options = {
+  host: "api.smartdropbox.online",   # Replace with your actual host
+  protocol: "https",    # Use 'http' or 'https' depending on your setup
+}
