c64757f1047a6d91db15232cbc0576afa02708ab
Arie A <dropbox@mountrash.com>
2023-09-09 19:50:03 +0700

    Local storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
   # config.active_storage.service = :local
-  config.active_storage.service = :cloudinary
+  config.active_storage.service = :local
   config.active_storage.draw_routes = false
 
   # Mount Action Cable outside main process or domain.
