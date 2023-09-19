f6f471d98563beda03a89c18eb43cd5d3059056c
Arie A <dropbox@mountrash.com>
2023-09-19 14:52:21 +0700

    Update manual transfer

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -171,18 +171,41 @@ class Api::V1::AdminController < AdminController
     end
   end
 
+  def bsi_show
+    bsi = Banksampah.find_by(uuid: params[:banksampah_uuid])
+    if bsi
+      render json: BankSampahBlueprint.render(bsi, view: :full)
+    else
+      render json: { message: "Record not found" }, status: :not_found
+    end
+  end
+
   def banksampah_datatable
     render json: BanksampahDatatable.new(params)
   end
 
   def manual_transfer
     bsi = Banksampah.find_by(uuid: params[:banksampah_uuid])
-    if params[:amount] >= 10000 && bsi
+    if params[:amount].to_i >= 10000 && bsi
       description = "Manual Tf, #{params[:description]}"
       bsi.mountpay_creditkan(params[:amount], description)
       render json: { message: "Success", balance: bsi.mountpay.balance }
     else
       render json: { message: "Record not found or amount cannot below 10000" }, status: :not_found
     end
+  end
+
+  def bsi_update
+    bsi = Banksampah.find_by(uuid: params[:banksampah_uuid])
+    if bsi.update(bsi_params)
+    else
+      render json: { message: "Record not found" }, status: :not_found
+    end
+  end
+
+  private
+
+  def bsi_params
+    params.permit(:phone, :name, :active)
   end
 end
