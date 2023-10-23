8554e724062eacf1eef8a5698ca4c70e62c82a06
Arie A <dropbox@mountrash.com>
2023-10-23 23:08:12 +0700

    Add UUID

--- app/blueprints/jemputan_blueprint.rb
+++ app/blueprints/jemputan_blueprint.rb
@@ -1,6 +1,6 @@
 class JemputanBlueprint < Blueprinter::Base
     identifier :id
-    fields :catatan, :status, :tanggal, :total, :fee
+    fields :catatan, :status, :tanggal, :total, :fee, :uuid
     association :alamat_jemput, blueprint: AlamatJemputBlueprint
     association :jemputan_details, blueprint: JemputanDetailBlueprint
 end
