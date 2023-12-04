0c8f22021e8bd587691d527d7f0812a48ad90430
Arie A <dropbox@mountrash.com>
2023-12-04 15:24:27 +0700

    Add active kolom

--- app/models/tipe_sampah.rb
+++ app/models/tipe_sampah.rb
@@ -10,17 +10,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-#  == Schema Information
-#
-# Table name: tipe_sampahs
-#
-#  id         :bigint           not null, primary key
-#  harga      :float(24)
-#  image_url  :string(255)
-#  name       :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
+
 class TipeSampah < ApplicationRecord
   validates :name, length: { in: 1..35 }, presence: true
   validates :harga, presence: true
