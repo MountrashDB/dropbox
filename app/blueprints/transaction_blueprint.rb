337afb00063920d1cd3a021bc7f81945f505f024
Arie A <dropbox@mountrash.com>
2023-12-07 16:18:37 +0700

    Add Status in trx blueprint

--- app/blueprints/transaction_blueprint.rb
+++ app/blueprints/transaction_blueprint.rb
@@ -2,7 +2,7 @@ class TransactionBlueprint < Blueprinter::Base
     identifier :uuid
 
     view :check_botol do
-        fields :harga, :diterima, :created_at
+        fields :harga, :diterima, :created_at, :status
         field :foto do |data|
             if data
                 data.foto.url
